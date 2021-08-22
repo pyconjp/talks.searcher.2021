bab8c29098fd0991667a5f6725671585b9f79d86
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 14:50:22 +0900

    [feat] Implement Category factory

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 
 def get_single_choice_category_value(category: dict) -> str:
     """sessionizeのAPIの返り値から単一選択項目の選択値を返す"""
     return category["categoryItems"][0]["name"]
 
 
+@dataclass
 class Category:
+    track: str
+    level: str
+    speaking_language: str
+    slide_language: str
+
     @staticmethod
     def flatten_raw_json(categories: list[dict]) -> dict[str, str]:
         """sessionizeのAPIの返り値のうち、ネストしたcategoriesフィールドをパースして平坦にする"""
@@ -32,3 +40,7 @@ class Category:
             "speaking_language": speaking_language,
             "slide_language": slide_language,
         }
+
+    @classmethod
+    def from_raw_json(cls, categories):
+        return cls(**cls.flatten_raw_json(categories))
