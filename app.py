bdb859fc4582f253fbd90473be7e5573f9ce4837
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 14:20:48 +0900

    [feat] Implement Category.flatten_raw_json

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
+from __future__ import annotations
+
+
+def get_single_choice_category_value(category: dict) -> str:
+    """sessionizeのAPIの返り値から単一選択項目の選択値を返す"""
+    return category["categoryItems"][0]["name"]
+
+
 class Category:
     @staticmethod
-    def flatten_raw_json(categories):
-        raise NotImplementedError
+    def flatten_raw_json(categories: list[dict]) -> dict[str, str]:
+        """sessionizeのAPIの返り値のうち、ネストしたcategoriesフィールドをパースして平坦にする"""
+        for category in categories:
+            if category["name"] == "Track":
+                track = get_single_choice_category_value(category)
+                continue
+            if category["name"] == "Level":
+                level = get_single_choice_category_value(category)
+                continue
+            if category["name"] == "Language":
+                speaking_language = get_single_choice_category_value(category)
+                continue
+            if (
+                category["name"]
+                == "発表資料の言語 / Language of presentation material"
+            ):
+                slide_language = get_single_choice_category_value(category)
+                continue
+        return {
+            "track": track,
+            "level": level,
+            "speaking_language": speaking_language,
+            "slide_language": slide_language,
+        }
