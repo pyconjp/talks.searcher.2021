58d520e1a71053721f4962a3b707105e191f4ed2
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 16:03:26 +0900

    [feat] Show talks as cards

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 from urllib.request import urlopen
 
 import streamlit as st
+import streamlit.components.v1 as components
 
 
 def get_single_choice_category_value(category: dict) -> str:
@@ -145,6 +146,68 @@ def fetch_talks(url):
     return data
 
 
+CARD_HTML_TEMPLATE = """
+<div class="card p-3">
+  <h5 class="card-title">{title} (by {speakers})</h5>
+  <h6 class="card-subtitle mb-2 text-muted">{elevator_pitch}</h6>
+  <div>
+    <dl>
+      <dt>Track</dt>
+      <dd>
+        <span class="badge rounded-pill bg-info text-dark">{track}</span>
+      </dd>
+      <dt>Python knowledge of audience</dt>
+      <dd>{python_level}</dd>
+      <dt>Language</dt>
+      <dd>{speaking_language} (Speak) / {slide_language} (Material)</dd>
+      <dt>Prior knowledges speakers assume the audience to have / 前提知識</dt>
+      <dd>{audience_prior_knowledge}</dd>
+      <dt>Knowledges and know-how the audience can get from talk
+      / 持ち帰れる知識・ノウハウ</dt>
+      <dd>{audience_take_away}</dd>
+    </dl>
+  </div>
+
+  <p>
+    <a class="btn btn-primary" data-bs-toggle="collapse"
+        href="#collapseDetail{i}" role="button" aria-expanded="false"
+        aria-controls="collapseExample">
+      Detail
+    </a>
+  </p>
+  <div class="collapse" id="collapseDetail{i}">
+    <div class="card card-body">
+      {description}
+    </div>
+  </div>
+</div>
+"""
+
+
+@st.cache
+def cards_html_to_talks(talks):
+    return "\n".join(
+        CARD_HTML_TEMPLATE.format(
+            i=i,
+            title=talk.title,
+            speakers=", ".join(speaker.name for speaker in talk.speakers),
+            track=talk.category.track,
+            python_level=talk.category.level,
+            speaking_language=talk.category.speaking_language,
+            slide_language=talk.category.slide_language,
+            description=talk.description.replace("\r\n", "<br>"),
+            elevator_pitch=talk.answer.elevator_pitch,
+            audience_prior_knowledge=talk.answer.audience_prior_knowledge.replace(  # NOQA
+                "\r\n", "<br>"
+            ),
+            audience_take_away=talk.answer.audience_take_away.replace(
+                "\r\n", "<br>"
+            ),
+        )
+        for i, talk in enumerate(talks)
+    )
+
+
 # ---------- Streamlit app ----------
 
 endpoint_id = st.secrets["ENDPOINT_ID"]
@@ -158,3 +221,17 @@ talks = Talks.from_raw_json(data[0]["sessions"])
 data_load_state.text("Loading Data... Done!")
 
 st.write("Found:", len(talks))
+
+cards_html = cards_html_to_talks(talks)
+components.html(
+    f"""
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    {cards_html}
+    """,
+    height=1000,
+    scrolling=True,
+)
